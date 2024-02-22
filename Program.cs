f9432fa844240ed55ab2e9318033dede10a35b9d
Michael W. Olesen <michael.w.olesen@gmail.com>
2024-02-22 11:35:36 +0100

    Temporary check in

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
-using ds_explorer;
+using DSE;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
