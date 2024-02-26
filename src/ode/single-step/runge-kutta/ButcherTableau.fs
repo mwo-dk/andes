521075767c41484a4d601d1e506e0aa15ac6e9c9
Michael W. Olesen <michael.w.olesen@gmail.com>
2024-02-27 00:19:46 +0100

    A little RK

---
+++ src/ode/single-step/runge-kutta/ButcherTableau.fs
@@ -0,0 +1,77 @@
+﻿namespace DSE.Ode.SingleStep.RungeKutta
+
+type ButcherTableau<'T> =
+    { A: 'T[][] // Coefficients for the stages
+      B: 'T[]   // Coefficients for the solution
+      C: 'T[]   // Coefficients for the time steps
+      Name: string // Name of the Butcher tableau
+    }
+
+type EmbeddedButcherTableau<'T> =
+    { A: 'T[][] // Coefficients for the stages
+      B1: 'T[]   // Coefficients for the solution
+      B2: 'T[]   // Coefficients for the solution
+      C: 'T[]   // Coefficients for the time steps
+      Name: string // Name of the Butcher tableau
+    }
+
+type Coefficient =
+| Z
+| N of int
+| R of float
+| Q of float*float
+| IQ of int*int
+
+[<AutoOpen>]
+module Helpers =
+
+    open System.Globalization
+
+    let private english = CultureInfo("en-US")
+    let private englishNumber = english.NumberFormat
+
+    let parse (s: string) =
+        if System.String.IsNullOrWhiteSpace(s) then
+            nullArg "s"
+        else
+            let items = s.Trim().Split([|'/'|], 
+                System.StringSplitOptions.RemoveEmptyEntries)
+            match items.Length with
+            | 1 -> 
+                match System.Int32.TryParse(items.[0]) with
+                | true, n -> if n = 0 then Z else  N n
+                | _ -> 
+                    match System.Double.TryParse(items.[0], englishNumber) with
+                    | true, r -> if r = 0.0 then Z else R r
+                    | _ -> Z
+            | 2 ->
+                match System.Int32.TryParse(items.[0]), System.Int32.TryParse(items.[1]) with
+                | (true, a), (true, b) -> 
+                    if b = 0 then invalidArg "s" "Denominator cannot be zero"
+                    else
+                        if a = 0 then Z else IQ (a, b)
+                | _ -> 
+                    match System.Double.TryParse(items.[0], englishNumber), 
+                        System.Double.TryParse(items.[1], englishNumber) with
+                    | (true, a), (true, b) ->
+                        if b = 0.0 then invalidArg "s" "Denominator cannot be zero" 
+                        else 
+                            if a = 0.0 then Z else Q (a, b)
+                    | _ -> Z
+            | _ -> invalidArg "s" "Invalid format"
+
+    let inline toFloat (x: Coefficient) =
+        match x with
+        | N n -> float n
+        | R d -> d
+        | Q (a, b) -> a / b
+        | IQ (a, b) -> float a / float b
+        | _ -> 0.0
+
+    let inline toFloat32 (x: Coefficient) =
+        match x with
+        | N n -> float32 n
+        | R d -> float32 d
+        | Q (a, b) -> float32 a / float32 b
+        | IQ (a, b) -> float32 a / float32 b
+        | _ -> 0.0f
