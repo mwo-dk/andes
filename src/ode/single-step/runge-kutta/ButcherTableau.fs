ca3b93f73ed5c2b1702c8f02abeaa35df17b586d
Michael W. Olesen <michael.w.olesen@gmail.com>
2024-02-27 12:40:01 +0100

    More butcher tableau

--- src/ode/single-step/runge-kutta/ButcherTableau.fs
+++ src/ode/single-step/runge-kutta/ButcherTableau.fs
@@ -1,5 +1,7 @@
 ﻿namespace DSE.Ode.SingleStep.RungeKutta
 
+open System.Linq.Expressions
+
 type ButcherTableau<'T> =
     { A: 'T[][] // Coefficients for the stages
       B: 'T[]   // Coefficients for the solution
@@ -21,9 +23,34 @@ type Coefficient =
 | R of float
 | Q of float*float
 | IQ of int*int
+with
+    member this.IsInfinite =
+        match this with
+        | R r -> System.Double.IsInfinity r
+        | Q (a, b) -> System.Double.IsInfinity a || System.Double.IsInfinity b || b = 0.0
+        | IQ (a, b) -> b = 0
+        | _ -> false
+    member this.IsZero =
+        match this with
+        | Z -> true
+        | N n when n = 0 -> true
+        | R r when r = 0.0 -> true
+        | Q (a, b) when a = 0.0 && b <> 0.0 -> true
+        | IQ (a, b) when a = 0 && b <> 0 -> true
+        | _ -> false
 
-[<AutoOpen>]
-module Helpers =
+//type CoefficientButcherTableau = ButcherTableau<Coefficient>
+//with
+//    member this.IsInfinite =
+//        this.A |> Array.exists (fun row -> row |> Array.exists (fun x -> x.IsInfinite)) ||
+//        this.B |> Array.exists (fun x -> x.IsInfinite) ||
+//        this.C |> Array.exists (fun x -> x.IsInfinite)
+//    member this.IsZero =
+//        this.A |> Array.exists (fun row -> row |> Array.exists (fun x -> x.IsZero)) ||
+//        this.B |> Array.exists (fun x -> x.IsZero) ||
+//        this.C |> Array.exists (fun x -> x.IsZero)
+
+module CoefficientHelpers =
 
     open System.Globalization
 
@@ -75,3 +102,62 @@ module Helpers =
         | Q (a, b) -> float32 a / float32 b
         | IQ (a, b) -> float32 a / float32 b
         | _ -> 0.0f
+
+    let toFloatArray (x: Coefficient[]) =
+        Array.map toFloat x
+
+    let toFloat32Array (x: Coefficient[]) =
+        Array.map toFloat32 x
+
+    let toFloatMatrix (x: Coefficient[][]) =
+        Array.map (fun row -> Array.map toFloat row) x
+
+    let toFloat32Matrix (x: Coefficient[][]) =
+        Array.map (fun row -> Array.map toFloat32 row) x
+
+module ButcherTableauHelpers =
+
+    open CoefficientHelpers
+
+    let create (a: Coefficient[][]) (b: Coefficient[]) (c: Coefficient[]) (name: string) =
+        { A = a
+          B = b
+          C = c
+          Name = name }
+
+    let toFloatButcherTableau (x: ButcherTableau<Coefficient>) =
+        { A = toFloatMatrix x.A
+          B = toFloatArray x.B
+          C = toFloatArray x.C
+          Name = x.Name }
+
+    let toFloat32ButcherTableau (x: ButcherTableau<Coefficient>) =
+        { A = toFloat32Matrix x.A
+          B = toFloat32Array x.B
+          C = toFloat32Array x.C
+          Name = x.Name }
+
+module EmbeddedButcherTableauHelpers =
+
+    open CoefficientHelpers 
+
+    let create (a: Coefficient[][]) (b1: Coefficient[]) (b2: Coefficient[]) (c: Coefficient[]) (name: string) =
+        { A = a
+          B1 = b1
+          B2 = b2
+          C = c
+          Name = name }
+
+    let toFloatEmbeddedButcherTableau (x: EmbeddedButcherTableau<Coefficient>) =
+        { A = toFloatMatrix x.A
+          B1 = toFloatArray x.B1
+          B2 = toFloatArray x.B2
+          C = toFloatArray x.C
+          Name = x.Name }
+
+    let toFloat32EmbeddedButcherTableau (x: EmbeddedButcherTableau<Coefficient>) =
+        { A = toFloat32Matrix x.A
+          B1 = toFloat32Array x.B1
+          B2 = toFloat32Array x.B2
+          C = toFloat32Array x.C
+          Name = x.Name }
