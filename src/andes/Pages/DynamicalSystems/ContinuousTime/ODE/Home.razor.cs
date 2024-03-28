123068f5505eb5fe41c1e5ebd4343d62333f152f
Michael W. Olesen <michael.w.olesen@gmail.com>
2024-03-28 12:12:17 +0100

    Starting ODE

---
+++ src/andes/Pages/DynamicalSystems/ContinuousTime/ODE/Home.razor.cs
@@ -0,0 +1,8 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Andes.Pages.DynamicalSystems.ContinuousTime.ODE;
+
+public partial class Home 
+{
+
+}
---
+++ src/dynamical-systems/continuous-time/ode/Library.fs
@@ -0,0 +1,47 @@
+﻿namespace Andes.DynamicalSystems.ContinuousTime.Ode
+
+open System
+open System.Linq.Expressions
+open System.Runtime.CompilerServices
+open Andes
+
+type Parameter = {
+    Name: string
+    Value: Number
+}
+type ParameterSet = Parameter list
+[<Struct>]
+type State<'T> = 
+| Scalar of s: 'T
+| Vector of v: 'T array
+
+type OrdinayDifferentialEquation<'T> = 
+| Autonomous of Expression<Func<State<'T>, State<'T>>>
+| NonAutonomous of Expression<Func<State<'T>, 'T, State<'T>>>
+
+[<Extension>]
+type OrdinaryDifferentialEquationExtensions =
+    [<Extension>]
+    static member inline IsAutonomous 
+        (ode: OrdinayDifferentialEquation<'T>) = 
+        match ode with
+        | Autonomous _ -> true
+        | _ -> false
+
+    [<Extension>]
+    static member inline IsNonAutonomous 
+        (ode: OrdinayDifferentialEquation<'T>) = 
+        match ode with
+        | NonAutonomous _ -> true
+        | _ -> false
+
+module OrdinaryDifferentialEquationRegistry =
+    let private registry = 
+        ResizeArray<OrdinayDifferentialEquation<Number>>()
+
+    let Register (ode: OrdinayDifferentialEquation<Number>) = 
+        registry.Add(ode)
+
+    let GetRegisteredOdes () = 
+        registry.ToArray()
+    
